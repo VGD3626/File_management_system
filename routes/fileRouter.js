767b175d90febdda9cb6cd50abfbcebe8baee119
Jaimin-07 <145255088+Jaimin-07@users.noreply.github.com>
2024-08-22 15:31:37 +0530

    fileRouter.js

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -6,5 +6,7 @@ router.post('/', fileControllers.createFile);
 router.get('/:id', fileControllers.getFileById);
 router.put('/:id', fileControllers.updateFileById);
 router.delete('/:id', fileControllers.deleteFileById);
+router.post('/:fileId/share', fileControllers.shareFileWithUser);
+router.get('/shared/:userId', fileControllers.getSharedFilesForUser);
 
 module.exports = router;
